1fd06c4a3924c28af076dc791bb3b52340e4592a
DDMeaqua <lzlloveyou@vip.qq.com>
2024-05-22 17:14:42 +0800

    fix: css

--- src/smoke.jsx
+++ src/smoke.jsx
@@ -16,13 +16,13 @@ const Smoke = () => {
                 {animate && (
                     <motion.div
                         className="smoke-effect"
-                        initial={{ scale: 0.8, opacity: 1 }}
+                        initial={{ scale: 0.9, opacity: 1 }}
                         animate={{
-                            scale: [0.9 , 1, 1.1],
+                            scale: [0.9 , 1.1, 1.1],
                             opacity: [1, 0.5, 0],
-                            boxShadow: ["0px 0px 10px 10px rgba(7, 241, 44, 0)", "0px 0px 20px 20px rgba(7, 241, 44, 0.6)", "0px 0px 30px 30px rgba(7, 241, 44, 0.8)"]
+                            boxShadow: ["0px 0px 0px 0px rgba(7, 241, 44, 0)", "0px 0px 20px 20px rgba(7, 241, 44, 0.6)", "0px 0px 20px 40px rgba(7, 241, 44, 0.8)"]
                         }}
-                        transition={{ duration: 0.7, ease: "easeOut" }}
+                        transition={{ duration: 1, ease: "easeOut" }}
                     />
                 )}
                 <div className="smoke-box" />

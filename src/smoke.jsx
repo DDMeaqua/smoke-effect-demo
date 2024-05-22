dc7527cffd428cdf0d938441c83a87baccbb0b96
DDMeaqua <lzlloveyou@vip.qq.com>
2024-05-22 16:16:44 +0800

    feat: init

--- src/smoke.jsx
+++ src/smoke.jsx
@@ -13,20 +13,19 @@ const Smoke = () => {
     return (
         <div className="container">
             <div className='bigbox'>
-                <div className="smoke-box">
-                    {animate && (
-                        <motion.div
-                            className="smoke-effect"
-                            initial={{ scale: 0.8, opacity: 1 }}
-                            animate={{
-                                scale: [1, 1.5],
-                                opacity: [1, 0],
-                                boxShadow: ["0px 0px 10px rgba(0, 0, 0, 0.5)", "0px 0px 20px rgba(0, 0, 0, 0)"]
-                            }}
-                            transition={{ duration: 1, ease: "easeOut" }}
-                        />
-                    )}
-                </div>
+                {animate && (
+                    <motion.div
+                        className="smoke-effect"
+                        initial={{ scale: 0.8, opacity: 1 }}
+                        animate={{
+                            scale: [1, 1.5, 1.5],
+                            opacity: [1, 0.5, 0],
+                            boxShadow: ["0px 0px 10px 10px rgba(7, 241, 44, 0)"]
+                        }}
+                        transition={{ duration: 1, ease: "easeOut" }}
+                    />
+                )}
+                <div className="smoke-box" />
             </div>
             <button onClick={handleClick} className="trigger-button">点击我</button>
         </div>
